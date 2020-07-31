cf5d93dbd96c2ba7dd6b89070ff947256a7a6ecd
Jeremias ramirez <jereramirez499@gmail.com>
2020-07-31 00:35:01 -0400

    agregando fechas a items

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -12,6 +12,14 @@ const routes: Routes = [
         path: 'notes',
         loadChildren: () => import('../pages/notes/notes.module').then( m => m.NotesPageModule)
       },
+        {
+        path: 'trash',
+        loadChildren: () => import('../pages/trash/trash.module').then( m => m.TrashPageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../pages/settings/settings.module').then( m => m.SettingsPageModule)
+      }
     ] 
   }
    
