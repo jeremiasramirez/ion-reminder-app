b7e1ff51b534abfcad488f5d51ff3890c522c9fd
Jeremias ramirez <jereramirez499@gmail.com>
2020-08-03 10:33:20 -0400

    mejorando pantalla de lock + agregado de imagenes de lock

--- src/app/pages/sign/sign.page.ts
+++ src/app/pages/sign/sign.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign.page.scss'],
 })
 export class SignPage implements OnInit {
-
+  public pass = ""
   constructor() { }
 
   ngOnInit() {
+  }
+  analizePass(){
+    if(this.pass.length === 4 ){
+      console.log("tiene 4 digitos")
+      if (this.pass === "0847"){
+        console.log("acepted")
+      }
+    }
+   
   }
 
 }
