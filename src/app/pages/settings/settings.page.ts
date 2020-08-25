f58289006aa9ef0364fe2217416478c9737cca0c
Jeremias ramirez <jereramirez499@gmail.com>
2020-08-25 09:11:06 -0400

    Cambiando iconos

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -16,3 +16,75 @@ export class SettingsPage implements OnInit {
   }
 
 }
+
+
+let settings =  [
+
+  {
+    name: "Pin",
+    subname: "Agregar, Eliminar",
+    router:"/home/settings/lock",
+    icon: "lock-closed"
+  },
+  {
+    name: "Vacear notas",
+    subname: "Analizar, Vacear papelera",
+    router:"/home/settings/analize",
+    icon: "speedometer"
+  },
+  {
+    name: "Notificación",
+    subname: "Activar, Preferencias de notificaciones",
+    router:"/home/settings/notification",
+    icon: "notifications"
+  },
+  {
+    name: "Plan",
+    subname: "Basic, Premium",
+    router:"/home/settings/plan",
+    icon: "flash"
+  },
+
+  {
+    name: "Reportar",
+    subname: " Fallos, Agregar mejoras",
+    router:"/home/settings/report",
+    icon: "notifications"
+  },
+
+  {
+    name: "Licence",
+    subname: "Información legal",
+    router:"/home/settings/licence",
+    icon: "document-outline"
+  },
+  {
+    name: "Glosario de iconos",
+    subname: "",
+    router:"/home/settings/glosario",
+    icon: "clipboard-outline"
+  },
+  {
+    name: "Mi phone",
+    subname: "Información, versión",
+    router:"/home/settings/phone",
+    icon: "hardware-chip"
+  }
+  
+]
+  
+let settingsClick =[
+  {
+    name: "Tema",
+    subname: "Dark, Light",
+    func:"changeTheme()",
+    icon: "moon"
+  },
+
+  {
+    name: "Resetear ajustes",
+    subname: "Tema, Notificaciones, Pin",
+    func:"resetSettings()",
+    icon: "reload"
+  }
+]
